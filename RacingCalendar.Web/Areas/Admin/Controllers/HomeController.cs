9959be1879908f26b365e78f09c11cf734872678
Lachezar_Donchev <stu2301321081@uni-plovdiv.bg>
2025-07-22 16:26:15 +0300

    Design of Admin Dashboard

--- RacingCalendar.Web/Areas/Admin/Controllers/HomeController.cs
+++ RacingCalendar.Web/Areas/Admin/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RacingCalendar.Data;
 
 namespace RacingCalendar.Web.Areas.Admin.Controllers
 {
@@ -7,9 +9,37 @@ namespace RacingCalendar.Web.Areas.Admin.Controllers
     [Authorize(Roles = "Administrator")]
     public class HomeController : Controller
     {
+        private readonly RacingCalendarDbContext _context;
+
+        public HomeController(RacingCalendarDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction("Dashboard");
+        }
+        public async Task<IActionResult> Dashboard()
+        {
+            try
+            {
+                ViewBag.TotalCircuits = await _context.Circuits.CountAsync();
+                ViewBag.TotalDrivers = await _context.Drivers.CountAsync();
+                ViewBag.TotalTeams = await _context.Teams.CountAsync();
+                ViewBag.UpcomingRaces = await _context.Races
+                    .Where(r => r.Date >= DateTime.Today)
+                    .CountAsync();
+            }
+            catch
+            {
+                ViewBag.TotalCircuits = 0;
+                ViewBag.TotalDrivers = 0;
+                ViewBag.TotalTeams = 0;
+                ViewBag.UpcomingRaces = 0;
+            }
+
+            return View(); 
         }
     }
 }
