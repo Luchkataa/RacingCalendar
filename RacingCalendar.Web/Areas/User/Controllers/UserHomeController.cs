787097d9558cd8922d796f6aac1acd7744f68690
Lachezar_Donchev <stu2301321081@uni-plovdiv.bg>
2025-07-28 19:16:50 +0300

    Changed the design of the User Index page

--- RacingCalendar.Web/Areas/User/Controllers/UserHomeController.cs
+++ RacingCalendar.Web/Areas/User/Controllers/UserHomeController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace RacingCalendar.Web.Areas.User.Controllers
 {
     [Area("User")]
     public class UserHomeController : Controller
     {
-        public IActionResult Index()
+        private readonly RacingCalendarDbContext _context;
+        public UserHomeController(RacingCalendarDbContext context)
         {
+            _context = context;
+        }
+        public async  Task<IActionResult> Index()
+        {
+            try
+            {
+                ViewBag.TotalCircuits = await _context.Circuits.CountAsync();
+                ViewBag.TotalDrivers = await _context.Drivers.CountAsync();
+                ViewBag.TotalTeams = await _context.Teams.CountAsync();
+                ViewBag.UpcomingRaces = await _context.Races
+                    .Where(r => r.Date >= DateTime.Today)
+                    .CountAsync();
+            }
+            catch
+            {
+                ViewBag.TotalCircuits = 0;
+                ViewBag.TotalDrivers = 0;
+                ViewBag.TotalTeams = 0;
+                ViewBag.UpcomingRaces = 0;
+            }
+
             return View();
         }
     }
